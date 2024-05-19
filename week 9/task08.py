fc2a8d26a4ecc66b17d0bd8ed7540555382a8b86
abhaykumardobariya <162405533+abhaykumardobariya@users.noreply.github.com>
2024-05-19 23:35:04 +1000

    Update task08.py

--- week 9/task08.py
+++ week 9/task08.py
@@ -1,15 +1,1 @@
 
-# Week-8 | Explanation of all the tasks in week-8
-
-## Task-1 | Display Menu
-
-### Code : 
-⁠ py
-def display_menu():
-    print("COIT20245 - Assignment 2")
-    print("\nMenu : ")
-    print("a. Help Menu")
-    print("b. Exit")
-
-if __name__ == "__main__":
-    display_menu()
