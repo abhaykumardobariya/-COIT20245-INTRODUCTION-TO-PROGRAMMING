31a50433b3ad9430f37185413cd925073d36e8a4
rahucqu <162515194+rahucqu@users.noreply.github.com>
2024-05-19 23:26:50 +1000

    Update task02.py

--- week 8/task02.py
+++ week 8/task02.py
@@ -1,4 +1,4 @@
-# Task-2
+py
 def main():
 
     display_menu()
@@ -8,41 +8,12 @@ def main():
 
         if user_input.lower() == "help":
             display_menu()
-
         elif user_input.lower() == "exit":
-            print("\nExiting the program.")
+            print("Exiting the program.")
             break
-
-        elif user_input.lower().startswith("species"):
-            parts = user_input.split(" ")
-            if len(parts) < 2:
-                print("Invalid command format. Please use 'species <city>' or 'species <city> venomous'")
-            else:
-                city = parts[1]
-                if len(parts) > 2 and parts[2].lower() == "venomous":
-                    species_list = search_species(city)
-                    venomous_species = filter_venomous(species_list)
-                    
-                    if venomous_species:
-                        display_species(venomous_species)
-                    else:
-                        print("Venomous Species not found!")
-
-                else:
-                    species_list = search_species(city)
-                    display_species(species_list)
-
-        elif user_input.lower().startswith("sighting"):
-            parts = user_input.split(" ")
-            if len(parts) != 3:
-                print("\nInvalid command format. Please enter the command in the format: sighting <city name> <Taxon ID>")
-            else:
-                _, city, taxonid = parts
-                sightings = search_sightings(taxonid, city)
-                display_sightings(sightings)
-            
         else:
-            print("Error: Invalid command")
+            print("Error: Invalid command. Please enter 'help' or 'exit'.")
 
-if _name_ == "_main_":
+
+if __name__ == "__main__":
     main()
