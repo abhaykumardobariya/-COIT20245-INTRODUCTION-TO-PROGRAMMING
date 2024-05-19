6b30bcd2783b6f713fb78c826891cbf6912485ed
abhaykumardobariya <162405533+abhaykumardobariya@users.noreply.github.com>
2024-05-19 21:16:59 +1000

    Update task04.py

--- week 8/task04.py
+++ week 8/task04.py
@@ -1,1 +1,14 @@
+# Task-4 - requirements
+def search_sightings(taxonid, city):
+    # Stub implementation
+    return [{ "properties": { "TaxonID":"860","StartDate":"1999-11-15","LocalityDetails":"Tinaroo","SiteCode":"INCIDENTAL" } }]
+
+
+def display_sightings(sightings):
+    print("\nAnimal sightings:")
+    for sighting in sightings:
+        start_date = sighting["properties"]["StartDate"]
+        locality = sighting["properties"]["LocalityDetails"]
+        print(f"Animal was sighted in {locality} on {start_date}\n")
+
 
