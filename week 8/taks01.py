b37f32359321231e66c6e2cb403f1ff25dbfd3d0
rahucqu <162515194+rahucqu@users.noreply.github.com>
2024-05-19 23:25:34 +1000

    Update taks01.py

--- week 8/taks01.py
+++ week 8/taks01.py
@@ -1,9 +1,9 @@
-# Task-1 
+py
 def display_menu():
-    print("\nCOIT20245 - Assignment 2")
+    print("COIT20245 - Assignment 2")
     print("\nMenu : ")
     print("a. Help Menu")
-    print("b. Animal species in a city - use 'species <city>' or 'species <city> venomous' commands")
-    print("c. Animal Sighting in a city - enter the command in the format : 'sighting <city name> <Taxon ID>'")
-    print("d. Exit")
+    print("b. Exit")
 
+if __name__ == "__main__":
+    display_menu()
