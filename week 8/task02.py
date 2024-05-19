8019e60bbd25567d5d302ed8be68b111b406c07c
abhaykumardobariya <162405533+abhaykumardobariya@users.noreply.github.com>
2024-05-19 22:59:36 +1000

    Update task02.py

--- week 8/task02.py
+++ week 8/task02.py
@@ -1,10 +1,4 @@
-# Task-1 
-def display_menu():
-    print("\nCOIT20245 - Assignment 2")
-    print("\nMenu : ")
-    print("a. Help Menu")
-    print("b. Exit")
-
+# Task-2
 def main():
 
     display_menu()
@@ -14,12 +8,41 @@ def main():
 
         if user_input.lower() == "help":
             display_menu()
-        elif user_input.lower() == "exit":
-            print("Exiting the program.")
-            break
-        else:
-            print("Error: Invalid command. Please enter 'help' or 'exit'.")
 
+        elif user_input.lower() == "exit":
+            print("\nExiting the program.")
+            break
+
+        elif user_input.lower().startswith("species"):
+            parts = user_input.split(" ")
+            if len(parts) < 2:
+                print("Invalid command format. Please use 'species <city>' or 'species <city> venomous'")
+            else:
+                city = parts[1]
+                if len(parts) > 2 and parts[2].lower() == "venomous":
+                    species_list = search_species(city)
+                    venomous_species = filter_venomous(species_list)
+                    
+                    if venomous_species:
+                        display_species(venomous_species)
+                    else:
+                        print("Venomous Species not found!")
+
+                else:
+                    species_list = search_species(city)
+                    display_species(species_list)
+
+        elif user_input.lower().startswith("sighting"):
+            parts = user_input.split(" ")
+            if len(parts) != 3:
+                print("\nInvalid command format. Please enter the command in the format: sighting <city name> <Taxon ID>")
+            else:
+                _, city, taxonid = parts
+                sightings = search_sightings(taxonid, city)
+                display_sightings(sightings)
+            
+        else:
+            print("Error: Invalid command")
 
 if _name_ == "_main_":
     main()
