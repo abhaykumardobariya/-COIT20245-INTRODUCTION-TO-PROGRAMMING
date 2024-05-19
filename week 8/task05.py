bf743f53eb14491edaed9e33e3da4725c4408396
rahucqu <162515194+rahucqu@users.noreply.github.com>
2024-05-19 23:19:50 +1000

    Update task05.py

--- week 8/task05.py
+++ week 8/task05.py
@@ -1,7 +1,10 @@
-# Task-5
 def filter_venomous(species_list):
     venomous_species = []
-    for entry in species_list:
-        if entry["PestStatus"] == "Venomous":
-            venomous_species.append(entry)
+    for species in species_list:
+        if species["Species"]["PestStatus"] == "Venomous":
+            venomous_species.append(species)
     return venomous_species
+
+def test_filter_venomous(venomous_species):
+    assert venomous_species == [{"Species":{"TaxonID":1040,"AcceptedCommonName":"snake","PestStatus":"Venomous"}}]
+    return True
