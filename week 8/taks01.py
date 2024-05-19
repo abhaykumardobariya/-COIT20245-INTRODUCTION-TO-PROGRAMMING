a2c9bed4b11c13077f34a639884d41cb060bc7a5
abhaykumardobariya <162405533+abhaykumardobariya@users.noreply.github.com>
2024-05-19 21:12:20 +1000

    Update taks01.py

--- week 8/taks01.py
+++ week 8/taks01.py
@@ -1,8 +1,25 @@
+# Task-1 
 def display_menu():
-    print("COIT20245 - Assignment 2")
+    print("\nCOIT20245 - Assignment 2")
     print("\nMenu : ")
     print("a. Help Menu")
     print("b. Exit")
 
-if _name_ == "_main_":
+def main():
+
     display_menu()
+
+    while True:
+        user_input = input("wildlife > ")
+
+        if user_input.lower() == "help":
+            display_menu()
+        elif user_input.lower() == "exit":
+            print("Exiting the program.")
+            break
+        else:
+            print("Error: Invalid command. Please enter 'help' or 'exit'.")
+
+
+if _name_ == "_main_":
+    main()
