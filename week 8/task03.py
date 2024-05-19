73b2343fe8da35b24137da263b52208f5e9f25de
abhaykumardobariya <162405533+abhaykumardobariya@users.noreply.github.com>
2024-05-19 23:00:08 +1000

    Update task03.py

--- week 8/task03.py
+++ week 8/task03.py
@@ -1,51 +1,24 @@
-# Task-1 
-def display_menu():
-    print("\nCOIT20245 - Assignment 2")
-    print("\nMenu : ")
-    print("a. Help Menu")
-    print("b. Animal species in a city")
-    print("c. Exit")
-
 # Task-3
 def search_species(city):
-    return [
-        {"Species":{"TaxonID":1039,"AcceptedCommonName":"dolphin","PestStatus":"Nil"}},
-        {"Species":{"TaxonID":1040,"AcceptedCommonName":"snake","PestStatus":"Venomous"}}
-    ]
+    RADIUS = 100000
+    coordiantes = gps(city)
+    # print(coordiantes)
+    species_list = get_species_list(coordiantes, RADIUS)
+    return species_list
 
 def display_species(species_list):
-
     print("\nDetail of species : \n")
-    for species in species_list:
-        print(
-            "TaxonID : " + str(species["Species"]["TaxonID"]) + "\n"
-            "Name of the species : " + species["Species"]["AcceptedCommonName"] + "\n"
-            "Pest Status : " + species["Species"]["PestStatus"] + "\n")
-
-# Task-2
-def main():
-  
-    display_menu()
-
-    while True:
-        user_input = input("wildlife > ")
-
-        if user_input.lower() == "help":
-            display_menu()
-        elif user_input.lower() == "exit":
-            print("Exiting the program.")
-            break
-        elif user_input.lower().startswith("species"):
-            parts = user_input.split(" ")
-            
-            if len(parts) != 2:
-                print("Error: Invalid command.")
-            else :
-                city = parts[1]
-                species_list = search_species
+    
+    for entry in species_list:
+        if "AcceptedCommonName" in entry:
+            print(
+                "TaxonID : " + str(entry["TaxonID"]) + "\n"
+                "Name of the species : " + entry["AcceptedCommonName"] + "\n"
+                "Pest Status : " + entry["PestStatus"] + "\n")
+            print("--"*30+"\n")
         else:
-            print("Error: Invalid command. Please enter 'help' or 'exit'.")
-
-
-if name == "main":
-    main()
+            print(
+                "TaxonID : " + str(entry["TaxonID"]) + "\n"
+                "Scientific Name of the species : " + entry["ScientificName"] + "\n"
+                "Pest Status : " + entry["PestStatus"] + "\n")
+            print("--"*30+"\n")
